a1153c4602e1ce22b75df90f313969fd11b94f01
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2021-06-24 18:14:20 +0200

    Update for version 1.0.1.

--- src/setup.py
+++ src/setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
 	name='wom_connector',
-	version='0.2',
+	version='1.0.1',
 	author='WOM Platform',
 	author_email='info@wom.social',
 	license='MIT',
-	description='Connection library to the WOM Platform.',
+	description='Connector library to the WOM Platform.',
 	long_description=open('README.txt').read(),
 	url="https://github.com/WOM-Platform/Python-Connector",
 	project_urls={
@@ -15,7 +15,7 @@ setuptools.setup(
 	},
 	packages=setuptools.find_packages(),
 	install_requires=[
-		'requests>=2.22.0',
-		'cryptography>=2.8'
+		'requests>=2.25.1',
+		'cryptography>=2.9.2'
 	]
 )
